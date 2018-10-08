538559712fade24cba41c4dc9601031db8e21572
Haley Jacobs <haleyljacobs@gmail.com>
2018-10-08 15:28:30 -0600

    Organized components files

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      people: [],
+      planets: [],
+      vehicles: []
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>SWAPI Box</h1>
         </header>
+
       </div>
     );
   }
